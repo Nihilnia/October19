7540ed765048316f0218d277dbe5fd88d12dfa01
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-10-01 15:21:38 +0300

    001_DoWhile

---
+++ 1_DoWhile_Ex/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DoWhile_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secim;
+
+            do
+            {
+                Console.WriteLine("1- Para çek\n2- Bakiye görüntüle\n3- Çıkış yap\nTerchiniz: ");
+                secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        Console.WriteLine("Hiç paranız yok lol!");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("0 liranız var lmao!");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Çıkış yapıldı.");
+                        break;
+
+                    default:
+                        Console.WriteLine("1-2-3 den birini seçin");
+                        break;
+                }
+            } while (secim != "3");
+            {
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
