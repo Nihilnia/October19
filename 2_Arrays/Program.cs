0c366e71a3b22508e079ba76dc6b6bc4e8e9c153
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-10-01 15:22:05 +0300

    002_Arrays

---
+++ 2_Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ogrNo = new int[20];
+            for (int i = 0; i < ogrNo.Length; i++)
+            {
+                ogrNo[i] = i + 1;
+            }
+
+            for (int i = 0; i < ogrNo.Length; i++)
+            {
+                Console.WriteLine("Ogr no: " + ogrNo[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
